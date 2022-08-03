5cb49fd8fa4639b4bc488a82e6249a5561aab57c
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-03 23:31:31 +0300

    Add currency and fix id type

--- ticket-service/src/main/java/com/microservicesdemo/ticketservice/model/Ticket.java
+++ ticket-service/src/main/java/com/microservicesdemo/ticketservice/model/Ticket.java
@@ -1,25 +1,49 @@
 package com.microservicesdemo.ticketservice.model;
 
-import lombok.Data;
+import com.microservicesdemo.ticketservice.dto.Currency;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 @Table(name = "ticket")
 public class Ticket extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", unique = true, nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String accountId;
+
+    @ManyToOne
+    @JoinColumn(name = "meetup_id", nullable = false)
+    private Meetup meetup;
+
 
     @Column(nullable = false)
     private Integer count;
 
     @Column(nullable = false)
     private LocalDateTime reserveDate;
+
+    @Enumerated
+    private Currency currency;
 
     @Column(nullable = false)
     private BigDecimal price;
