c3fd77c6cef31094a7194caa90e8f57b535a5c0e
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-03 23:36:49 +0300

    Implement IT for ticket repository

---
+++ ticket-service/src/test/resources/sql/cleanup.sql
@@ -0,0 +1,2 @@
+DELETE FROM ticket;
+DELETE FROM meetup;
---
+++ ticket-service/src/test/resources/sql/ticket.sql
@@ -0,0 +1,3 @@
+INSERT INTO meetup (id, created_at, updated_at, event_date, name, price, url, currency)
+VALUES ('36a57006-1362-11ed-8887-0242ac150002', localtime(), localtime(), localtime(), 'Encryption Security', 100,
+        'http://www.zoom.us/G4H9N9', 2);
