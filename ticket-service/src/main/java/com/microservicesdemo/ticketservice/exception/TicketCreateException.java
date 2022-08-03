60ff8c0d9d12a716aac8ff21506781ff4d963c76
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-03 23:35:12 +0300

    Add exception class

---
+++ ticket-service/src/main/java/com/microservicesdemo/ticketservice/exception/TicketCreateException.java
@@ -0,0 +1,17 @@
+package com.microservicesdemo.ticketservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+public class TicketCreateException extends RuntimeException {
+
+    private final String ticket;
+    private final String message;
+
+    public TicketCreateException(String ticket, String message) {
+        super(String.format("Failed to create Ticket[%s] : '%s'", ticket, message));
+        this.ticket = ticket;
+        this.message = message;
+    }
+}
