74df7d17fa1ccc88ec4f7dbbc6ee4a76b3c85a08
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-04 22:05:21 +0300

    Add discovery server for ticket-service

--- ticket-service/src/main/java/com/microservicesdemo/ticketservice/TicketApplication.java
+++ ticket-service/src/main/java/com/microservicesdemo/ticketservice/TicketApplication.java
@@ -2,7 +2,9 @@ package com.microservicesdemo.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TicketApplication {
     public static void main(String[] args) {
