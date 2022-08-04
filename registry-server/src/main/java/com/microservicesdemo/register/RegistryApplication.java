0125cacba9cc16fd9e68c5f5ff34190e808aeea6
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-04 19:54:39 +0300

    Refactored for module name

--- registry-server/src/main/java/com/microservicesdemo/register/RegistryApplication.java
+++ registry-server/src/main/java/com/microservicesdemo/register/RegistryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class RegisterApplication {
+public class RegistryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RegisterApplication.class, args);
+        SpringApplication.run(RegistryApplication.class, args);
     }
 }
