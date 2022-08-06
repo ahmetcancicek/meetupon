a8b1198890981b618682ee81cfe07003140dcc76
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-06 12:33:47 +0300

    Refactored package name

--- registry-server/src/main/java/com/microservicesdemo/registry/RegistryApplication.java
+++ registry-server/src/main/java/com/microservicesdemo/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package com.microservicesdemo.register;
+package com.microservicesdemo.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
