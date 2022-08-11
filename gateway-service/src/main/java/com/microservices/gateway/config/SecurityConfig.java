bcc2378c32bac0229b8cc9daac4da5420f4b1fdd
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-11 10:50:02 +0300

    Add security config for gateway-service

---
+++ gateway-service/src/main/java/com/microservices/gateway/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.microservices.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange()
+                .pathMatchers("/actuator/**")
+                .permitAll()
+                .and()
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .build();
+    }
+}
