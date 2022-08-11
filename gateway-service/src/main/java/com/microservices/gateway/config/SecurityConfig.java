28e949e37b12948ff6c425924bf80834a847935b
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-11 19:48:02 +0300

    Refactored config

--- gateway-service/src/main/java/com/microservices/gateway/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/microservices/gateway/config/SecurityConfig.java
@@ -3,25 +3,21 @@ package com.microservices.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange()
-                .pathMatchers("/actuator/**")
-                .permitAll()
-                .and()
-                .authorizeExchange()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .oauth2Login()
-                .and()
-                .build();
+        http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+        http.csrf().disable();
+
+        return http.build();
     }
 }
