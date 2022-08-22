24d480bd5d2b34c7e295d33330159d3accf53755
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-22 20:43:58 +0300

    Fix enums variables

--- auth-service/src/main/java/com/meetupon/auth/vo/RoleName.java
+++ auth-service/src/main/java/com/meetupon/auth/vo/RoleName.java
@@ -4,6 +4,14 @@ public enum RoleName {
     ROLE_USER("USER"),
     ROLE_ADMIN("ADMIN");
 
+    private final String name;
+
     RoleName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
